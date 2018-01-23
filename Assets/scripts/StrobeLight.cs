3cf433206661375ec9fca24fdb0c3c5b89b70754
TesMV <teselkin29@gmail.com>
2018-01-23 22:01:48 +0300

    Initial commit

---
+++ Assets/scripts/StrobeLight.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(Light))]
+
+public class StrobeLight : MonoBehaviour {
+
+	public float time = .5f; //time between on and off
+	
+	// Use this for initialization
+	void Start () {
+		StartCoroutine("Flicker");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	IEnumerator Flicker(){
+		while(true){
+			GetComponent<Light>().enabled = false;
+			yield return new WaitForSeconds(time);
+			GetComponent<Light>().enabled = true;
+			yield return new WaitForSeconds(time);
+		}
+	}
+}
